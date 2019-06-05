7be0b72e2dc8a6310737c0ac41b8a1c54c0dd649
Prashant <prashant4756@gmail.com>
2019-06-05 15:44:44 +0530

    test commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
-
+// testing
 const App = () => (
 	//component nesting
 	<Header headerTitle={'Albums'} />
