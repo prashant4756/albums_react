bca4607a90de476f6ca8fcf581c6f3eafcad65b7
Prashant <prashant4756@gmail.com>
2019-06-08 16:29:55 +0530

    button component/ button click/ deeplink to web application

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,31 +1,38 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
     //parameter is destructured, we are taking album key from prop 
 
     //this line destructures title, artist, thumbnail_image from album 
-    const { title, artist, thumbnail_image, image } = album;
-    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, albumCoverStyle } = styles;
+    const { title, artist, thumbnail_image, image, url } = album;
+    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, 
+        albumCoverStyle } = styles;
     console.log(title);
     return (
         <Card>
             <CardSection>
-            <View style={thumbnailContainerStyle}>
-                <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
-            </View>
+                <View style={thumbnailContainerStyle}>
+                    <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+                </View>
 
-            <View style={headerContentStyle} >
-                <Text style={headerTextStyle}> { title } </Text>
-                <Text> { artist } </Text>
-            </View>
+                <View style={headerContentStyle} >
+                    <Text style={headerTextStyle}> { title } </Text>
+                    <Text> { artist } </Text>
+                </View>
             </CardSection>
-
 
             <CardSection>
                 <Image style={albumCoverStyle} source={{ uri: image }} />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy {title}
+                </Button>
             </CardSection>
         </Card>
     );
@@ -53,7 +60,7 @@ const styles = {
         height: 300,
         flex: 1,
         width: null
-    },
+    }
 };
 
 
